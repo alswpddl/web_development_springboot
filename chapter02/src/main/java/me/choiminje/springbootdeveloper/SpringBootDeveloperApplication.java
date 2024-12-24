57a738ba2befceac82f2c6a861824fb286929042
alswpddl <alswp0207@gmail.com>
2024-12-24 19:47:22 +0900

    feat: chaper02

--- chapter02/src/main/java/me/choiminje/springbootdeveloper/SpringBootDeveloperApplication.java
+++ chapter02/src/main/java/me/choiminje/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -8,6 +8,8 @@ package me.choiminje.springbootdeveloper;
             -> SpringBootDeveloperApplication에서 @SpringBootApplication 애너테이션에 빨간줄이 가있다면
                 새로고침 안했을 확률이 거의 90%
         5. resources 내에 static 내에 index.html이라고 하는데, 해당 폴더명의 경우 대부분의 개발자들이 합의한 형태
+        6. 앞으로의 수업 시간에는 new project 생성시에 이렇게 자세히 풀이할 일은 자주 있지 않을 예정이니 꼭
+            익혀두시기 바랍니다. -> github 본다고 되는 부분이 아닌만큼 신경써주시면 감사하겠습니다.
  */
 
 import org.springframework.boot.SpringApplication;
@@ -17,22 +19,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
-
-        /*
-            처음으로 SpringBootDeveloperApplication 파일을 실행시키면 whiteLabel error page가 뜹니다.
-            현재 요청에 해당하는 페이지가 존재하지 않기 때문에 생격난 문제입니다.
-            -> 하지만 스프링 애플리케이션은 실행됨
-
-            그래서 error 페이지가 기분 나쁘니까 기본적으로 실행될 때의 default 페이지를 하나 생성하겠습니다.
-
-            20241223 MON.
-                1. IntelliJ Community Version 설치 -> bin.PATH 체크하고 나머지는 전부 디폴트
-                2. Git 설치
-                3. GItHub 연동 -> web_development_springboot -> 현재 문제가 좀 있음
-                4. IntelliJ에 Gradle 및 SpringBoot 프로젝트를 생성
-                5. POSTMAN을 설치
-         */
-
-
+        // 실험용
     }
 }
