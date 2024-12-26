c2a9a18c1b347d90958150476358ccf4a18fdcff
alswpddl <alswp0207@gmail.com>
2024-12-26 19:57:54 +0900

    feat: chapter03 생성

--- chapter03/src/main/java/me/choiminje/springbootdeveloper/Member.java
+++ chapter03/src/main/java/me/choiminje/springbootdeveloper/Member.java
@@ -9,16 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Entity
-
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false)      // 테이블 상에서의 컬럼 명이 id라는 의미
     private Long id;    // DB 테이블의 'id' 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false)    // 테이블 상에서의 컬럼 명이 name이라는 의미
     private String name;    // Db 테이블의 'name' 컬럼과 매칭
-    /*
+    /*g
         각 애너테이션의 역할을 lombok의 경우 java 과정에서 다 설명했습니다.
         하지만 추후 다시 설명드리겠습니다.
         현재는 member라는 이름의 테이블에 접근하는 데 사용하는 객체로 이해하세요.
