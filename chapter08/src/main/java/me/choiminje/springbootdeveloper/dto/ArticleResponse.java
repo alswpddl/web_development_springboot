81899b0bb3cf3b97d0b6f01539ac16f7d6fce34c
alswpddl <alswp0207@gmail.com>
2025-01-14 21:40:57 +0900

    feat: by making signup.html

---
+++ chapter08/src/main/java/me/choiminje/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,17 @@
+package me.choiminje.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.choiminje.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+}
